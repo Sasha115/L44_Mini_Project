43a653e6c3cb8171ecec1760eb754fa3bda1c7cb
Sasha115 <sasha.naidoo11@gmail.com>
2018-01-16 00:21:31 +0000

    Updated run.py

--- run.py
+++ run.py
@@ -26,13 +26,15 @@ height_image = resnet_v1.default_image_size
 width_image = resnet_v1.default_image_size
 print(height_image)
 
-def get_images(data_dir, training, batch_size=10):
+def get_images(data_dir, data_type, batch_size=10):
   height = height_image
   width = width_image
-  if(training == True):
+  if(data_type == 1):
     data_dir = data_dir +"Train/"
-  else:
+  elif(data_type == 2):
     data_dir = data_dir +"Val/"
+  elif(data_type == 3):
+    data_dir = data_dir +"Test/"
   input_files_positive = glob(data_dir + "Positive/*.jpg")
   input_files_negative = glob(data_dir + "Negative/*.jpg")
   input_files_neutral = glob(data_dir + "Neutral/*.jpg")
@@ -49,13 +51,26 @@ def get_images(data_dir, training, batch_size=10):
   while 1:
     image_names,labels = zip(*next(label_files_grouped))
     
-    #can change image size - note - possibly investigate?
-    image_files = [imresize(np.uint8(imageio.imread(fname)),(height,width)) for fname in image_names]
+    dx = 224
+    dy = 224
+    
+    image_data = [Image.open(fname) for fname in image_names]
+            
+    images = [image.resize((256,256),PIL.Image.ANTIALIAS) if (image.size[0] < 256 or image.size[1] < 256) else image for image in image_data]
+    
+    images_crop = []
+    for image in images:
+        x = random.randint(0, image.size[0]-dx-1)
+        y = random.randint(0, image.size[1]-dy-1) 
+        images_crop.append(image.crop((x,y, x+dx, y+dy)))
+    
+    pil_images = [np.fliplr(image) if np.random.random() > 0.5 else image for image in images_crop]
+    image_files = [np.array(np_image) for np_image in pil_images]
     
     yield zip(image_files,list(labels))
 
-data_dir = "/home/ubuntu/project/data/"
 
+data_dir = "/home/ubuntu/project/data/"
 total_num_images = 3630
 num_epochs = 100 #hp 1
 batch_size = 50 #hp 2
@@ -77,24 +92,32 @@ keep_prob = tf.placeholder(tf.float32,shape=[])
 
 with slim.arg_scope(resnet_arg_scope()):
     #restore resnet101 model
-    resnet_logits, _ = resnet_v1_101(images, num_classes=1000, global_pool=True, is_training=True)
+    resnet_logits, end_points = resnet_v1_101(images, num_classes=1000, global_pool=True, is_training=True)
     
 
-def feed_dict(batch_size, training, epoch):
+def feed_dict(batch_size, data_type, epoch):
   keep_prob_per = keep_prob_val
   lr = initial_learning_rate
-  if training == True:
-    data = get_images(data_dir,training,batch_size)
+    
+  if data_type == 1:
+    data = get_images(data_dir,data_type,batch_size)
     keep_prob_per = keep_prob_val
-  elif training == False:
-    data = get_images(data_dir,training,batch_size)
+
+  elif data_type == 2:
+    data = get_images(data_dir,data_type,batch_size)
     keep_prob_per = 1
+    
+  elif data_type == 3:
+    data = get_images(data_dir,data_type,batch_size)
+    keep_prob_per = 1
+    
   imgs, lbls = map(list, zip(*next(data)))
+
+  
   if(epoch >= num_epochs_before_decay and epoch % learning_rate_decay_iterations == 0):
     lr = 0.001*(10**(-epoch/100))
 
   return {images: imgs, labels: lbls, learning_rate: lr, keep_prob: keep_prob_per}
-
 
 #checkpoint_exclude_scopes=["resnet_v1_101/logits"]
 checkpoint_exclude_scopes=[]
@@ -113,7 +136,7 @@ for var in slim.get_model_variables():
 saver = tf.train.Saver(variables_to_restore)
 
 #introduce dropout for resnet final FC layer
-logits_drop = tf.contrib.layers.dropout(resnet_logits, keep_prob)
+logits_drop = tf.contrib.layers.dropout(end_points['global_pool'], keep_prob)
 #introduce final FC layer to map output of resnet 1000 to 3 classes
 logits = tf.contrib.layers.fully_connected(logits_drop, 3)
 
@@ -150,8 +173,8 @@ with tf.Session() as sess:
     start_time_per_epoch = time.time()
     for j in range(num_batches_per_epoch):
       print("Epoch %s, Batch %s" % (i,j))
-      _,training_loss = sess.run([train_op, cross_entropy],feed_dict=feed_dict(batch_size,True,i))
-      train_accuracy = accuracy.eval(feed_dict=feed_dict(batch_size,True,i))
+      _,training_loss = sess.run([train_op, cross_entropy],feed_dict=feed_dict(batch_size,1,i))
+      train_accuracy = accuracy.eval(feed_dict=feed_dict(batch_size,1,i))
       total_loss_val = total_loss_val + training_loss
       total_train_accuracy = total_train_accuracy + train_accuracy
     avg_training_acc = total_train_accuracy/num_batches_per_epoch
@@ -159,7 +182,7 @@ with tf.Session() as sess:
     print("Epoch %s: training accuracy %s, training loss %s" % (i,avg_training_acc,total_loss_val))
     time_elapsed_training_per_epoch = time.time() - start_time_per_epoch
     for _ in range(num_batches_per_epoch):
-      validation_accuracy = accuracy.eval(feed_dict=feed_dict(batch_size,False,i))
+      validation_accuracy = accuracy.eval(feed_dict=feed_dict(batch_size,2,i))
       total_val_accuracy = total_val_accuracy + validation_accuracy
     avg_val_acc = total_val_accuracy/num_batches_per_epoch
     print("Epoch %s: validation accuracy %s" % (i,avg_val_acc))
